decb2e3c3d7bc63584c7e8c1490394f0156ad054
fionn-the-goose <ericksoncooperation@gmail.com>
2025-04-10 13:20:06 +0200

    assingment 1.4 in progress, nearly dun but i will switch devices now

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -11,9 +11,9 @@ int gcd(int a, int b){
 }
 
 int gcd_assingment(){
-  int result;
-  for(int i = 1; i>=20 ; i++){
-      for (int j = 1; j >= 20; j++)
+  int result = 1000000000;
+  for(int i = 1; i<=20 ; i++){
+      for (int j = 1; j <= 20; j++)
       {
           int temp_gcd = gcd(i,j);
           if(temp_gcd > result){
@@ -21,13 +21,29 @@ int gcd_assingment(){
           }
       }
   }
+  std::cout << result; 
   return result;
 }
 
-int main()
-{
+//assingment 1.4.
+void pythagi(){
+  for(int a = 1; a>=999; a++){
+    for(int b = 1; b>=999; b++){
+      for(int c = 1; c>=999; c++){
+        if((a+b+c)== 1000){
+          if(a^2*b^2==c^2){
+            std::cout << "a: " << a << " ,b: " << b << ",c: " << c;
+          }
+        }
+      }
+    }
+  }
+  
+}
+
+int main(){
   std::cout << "Hi Programmiersprachen Kurs!!!\n";
   int result = gcd_assingment();
-  std::cout << result; 
+  pythagi();
   return 0;
 }
